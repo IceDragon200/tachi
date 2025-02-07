80bfd5738aa451f0304214be5e315e747a56f94b
Corey Powell <mistdragon100@gmail.com>
2025-02-07 11:46:53 -0500

    Change working directory to tachi's before execution

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@ root_dir=$(dirname "$(readlink -f "$0")")
 if ! [[ -z $1 ]]; then
 cat <<__EOF__ > $1
 #!/usr/bin/env bash
-${root_dir}/bin/tachi "\$@"
+cd "${root_dir}"
+exec ./bin/tachi "\$@"
 __EOF__
 chmod +x $1
 echo "Installed Tachi shim at: $1"
